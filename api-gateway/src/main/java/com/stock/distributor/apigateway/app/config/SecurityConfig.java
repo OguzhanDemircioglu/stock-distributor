8fefd26143d017c3cbeff295b588fa98f8909a8c
OguzhanAcer <oguzhanturgut611@gmail.com>
2024-11-14 02:11:45 +0300

    keycloack

---
+++ api-gateway/src/main/java/com/stock/distributor/apigateway/app/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.stock.distributor.apigateway.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(auth2-> auth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
